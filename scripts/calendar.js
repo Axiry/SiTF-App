e44ff006ad302cfcb7e98476c7cbdd22768e208c
Eiffel Chong <chong.shiangyih.eiffel@dhs.sg>
2017-04-10 16:58:16 +0800

    Update calendar.js

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,13 +1,13 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("sideNav").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+    document.body.style.backgroundColor = "#E8E8E8";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("sideNav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
 }
